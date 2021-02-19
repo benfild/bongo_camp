59d25fe3e3b32f63d9d6a7e3034a23715abc86ab
benfild <fildben@gmail.com>
2021-02-19 21:34:32 +0300

    loggin links

--- v7/app.js
+++ v7/app.js
@@ -35,7 +35,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+//pass currentUser in every route
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 //RESTFUL ROUTES
 app.get("/", function (req, res) {
@@ -50,7 +54,8 @@ app.get("/campgrounds", function (req, res) {
             console.log(err);
         } else {
             res.render("campgrounds/index", {
-                campGrounds: allCampgrounds
+                campGrounds: allCampgrounds,
+                currentUser: req.user
             });
         }
     });
@@ -104,7 +109,7 @@ app.get('/campgrounds/:id', function (req, res) {
 //===================
 // COMMENTS ROUTES
 //===================
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn,function (req, res) {
     //find campground by id
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
@@ -115,7 +120,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function (req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn,function (req, res){
     //find campground by id
     Campground.findById(req.params.id, function (err,campground){
         if (err) {
@@ -171,6 +176,19 @@ app.post("/login", passport.authenticate("local", {
 }),function(req, res){
 
 });
+//Logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(PORT, process.env.IP, function () {
     console.log("Server started at:" + PORT);
